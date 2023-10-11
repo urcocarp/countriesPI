8483731628391b25e625795da939e6bbc24b645a
urco <nik_186@hotmail.com>
2023-10-11 20:29:10 -0300

    actualizacion

---
+++ server/index.js
@@ -0,0 +1,29 @@
+const axios = require("axios");
+const server = require("./src/server");
+const { conn } = require('./src/db.js');
+const PORT = 3001;
+const getCountries = require ("./src/controllers/getCountries")
+const {Country} = require("./src/db.js")
+
+
+
+
+
+
+
+conn.sync({ force:true }).then(async() => {
+  try {
+  const countries = await getCountries();
+  await Country.bulkCreate(countries)
+// console.log(countries)
+
+} catch (error) {
+  console.error("error al cargar los datos",error);
+}
+
+
+
+server.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+})
+}).catch(error => console.error(error))
