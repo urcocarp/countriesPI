6f9f59e46ae9b611c15ff760bd13b7e188b97576
urco <nik_186@hotmail.com>
2024-01-25 13:23:09 -0300

    vercel

--- server/src/server.js
+++ server/src/server.js
@@ -6,6 +6,16 @@ const cors = require("cors");
 const server = express();
 
 server.use(morgan("dev"));
+server.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "https://countries-pi-omega.vercel.app/");
+    res.header("Access-Control-Allow-Credentials", "true");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+    next();
+  });
 server.use(express.json());
 server.use(cors());
 server.use(router);
