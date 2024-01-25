2411aaee601e70df6199ace5ab2acdbc01dffafd
urco <nik_186@hotmail.com>
2024-01-25 17:19:36 -0300

    aste

--- server/src/server.js
+++ server/src/server.js
@@ -16,13 +16,12 @@ server.use(bodyParser.json({ limit: '50mb' }));
 server.use(cookieParser());
 server.use(morgan('dev'));
 server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://countries-pi-omega.vercel.app/'); // update to match the domain you will make the request from
+  res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
   next();
 });
-
 server.use('/', router);
 
 // Error catching endware.
